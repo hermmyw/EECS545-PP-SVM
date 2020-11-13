b2d8ba578aa8f7b227497bd6abacd28abcf8a6e7
Huimin Wang <huiminwang@Huimins-MacBook-Pro-2.local>
2020-11-12 22:20:35 -0500

    [preprocessing] Data split and standardization

---
+++ src/dataloader.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+class DataLoader(object):
+    def __init__(self, file_path):
+        # import data and remove NaN samples
+        self.raw_data = np.genfromtxt(file_path, delimiter=',')
+        self.raw_data = self.raw_data[~np.isnan(self.raw_data).any(axis=1)]
+        
+        # extract features and labels
+        X = self.raw_data[:,0:-1]
+        y = self.raw_data[:,-1]
+        y = self.set_binary(y)
+        
+        # split and normalize
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        X_train, X_test = self.normalize(X_train, X_test)
+        self.data = (X_train, X_test, y_train, y_test)
+        
+    def normalize(self, X, T):
+        scaler = StandardScaler()
+        scaler.fit(X)
+        X = scaler.transform(X)
+        T = scaler.transform(T)
+        return X, T
+    
+    def set_binary(self, y, val1=-1, val2=1):
+        thr = (np.max(y) + np.min(y))/2
+        y[y <= thr] = val1
+        y[y > thr] = val2
+        return y
+        
+wbc_loader = DataLoader("../assets/breast_cancer_wisconsin.data")
+X_wbc_train, X_wbc_test, y_wbc_train, y_wbc_test = obj.data
+ddr_loader = DataLoader("../assets/messidor_features.data")
+X_ddr_train, X_ddr_test, y_ddr_train, y_ddr_test = obj.data
+
