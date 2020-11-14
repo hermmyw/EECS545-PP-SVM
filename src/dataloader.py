2c56e17737d401ceadefb02af1e76490e93bd7b7
QAQAQAQAQAQ <wenleyang@yeah.net>
2020-11-13 21:50:27 -0500

    train SVM and get needed parameters

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
+from sklearn import svm
 
 class DataLoader(object):
     def __init__(self, file_path):
@@ -31,8 +33,37 @@ class DataLoader(object):
         y[y > thr] = val2
         return y
         
-wbc_loader = DataLoader("../assets/breast_cancer_wisconsin.data")
-X_wbc_train, X_wbc_test, y_wbc_train, y_wbc_test = obj.data
-ddr_loader = DataLoader("../assets/messidor_features.data")
-X_ddr_train, X_ddr_test, y_ddr_train, y_ddr_test = obj.data
+def get_svm_weights(trainData,testData=None,p=2,c=3,verbose=True):
+    '''
+    :param trainData: (X_train, Y_train)
+    :param testData:  (X_test, Y_test)
+    :param p: order of poly (in paper p=2 for wbc dataset)
+    :return: support_vecs,intercept, dual_coef, model, perdiction_result(optional)
+    '''
+    clf = svm.SVC(kernel="poly",degree=p,C=c)
+    if verbose:
+        scores = cross_val_score(clf, trainData[0], trainData[1], cv=5)
+        print("val acc (leave one out among training data):",np.mean(scores))
+    clf.fit(trainData[0], trainData[1])
+    support_vecs = clf.support_vectors_
+    intercept = clf.intercept_
+    dual_coef = clf.dual_coef_
 
+    if testData !=None:
+        perdiction_result = clf.predict(testData[0])
+        acc = perdiction_result==testData[1]
+        acc = sum(acc)/acc.shape[0]
+        if verbose:
+            print("test acc:",acc)
+    return support_vecs,intercept, dual_coef,clf, perdiction_result
+
+if __name__ == '__main__':
+    wbc_loader = DataLoader("../assets/breast_cancer_wisconsin.data")
+    X_wbc_train, X_wbc_test, y_wbc_train, y_wbc_test = wbc_loader.data
+    ddr_loader = DataLoader("../assets/messidor_features.data")
+    X_ddr_train, X_ddr_test, y_ddr_train, y_ddr_test = ddr_loader.data
+
+    support_vecs,intercept, dual_coef, model,_ =get_svm_weights((X_wbc_train,y_wbc_train),(X_wbc_test,y_wbc_test))
+    support_vecs_ddr, intercept_ddr, dual_coef_ddr, model_ddr, _ = get_svm_weights((X_ddr_train, y_ddr_train), (X_ddr_test,
+                                                                                                y_ddr_test),p=3,c=0.3)
+    # Note: ddr dataset is probably not the best dataset for poly SVM, best test accuracy is around 65%
